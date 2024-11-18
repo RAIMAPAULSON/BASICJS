bf17ed6b35bf665c25b3d9a0e26ad0cb35cff1ef
RAIMAPAULSON <raimapaulson11@gmail.com>
2024-11-18 22:26:25 +0530

    project updated

---
+++ solid_rectPattern.js
@@ -0,0 +1,11 @@
+function pattern(n){
+let pattern = "";
+for(let i = 1; i <= n; i++){
+  for(let j = 1; j <= n; j++){
+    pattern += j + " ";
+  }
+  pattern += "\n";
+}
+return pattern;
+}
+console.log(pattern(4));
--- task15.js
+++ task15.js
@@ -1,4 +1,10 @@
+// function printRaimaKutoos(num) {
+//   console.log(("Raima Kuttoos\n").repeat(num));
+// }
+// printRaimaKutoos(5);
 function printRaimaKutoos(num) {
-  console.log(("Raima Kuttoos\n").repeat(num));
+  for(let i = 1; i <=5 ; i++){
+  console.log("Raima Kuttoos");
+  }
 }
 printRaimaKutoos(5);
